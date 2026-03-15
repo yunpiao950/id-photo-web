a98c2945be20f6e713566cbf68a8e4d40f5e47b5
yunpiao950 <yunpiao950@users.noreply.github.com>
2026-03-15 15:44:19 +0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/id-photo-web/',
   server: {
     host: '0.0.0.0',
     port: 3000,
